2afbdb39670ebd4157408564fd1900db614cc6cc
Pascal Hertleif <pascal@technocreatives.com>
2019-07-12 16:46:56 +0200

    CI stuff

--- fuzz/fuzz_targets/parse.rs
+++ fuzz/fuzz_targets/parse.rs
@@ -5,9 +5,9 @@ use std::str;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(utf8) = str::from_utf8(data) {
-        if let Ok(parsed) = url::Url::parse(utf8) {
+        if let Ok(parsed) = earl::Url::parse(utf8) {
             let as_str = parsed.as_str();
-            assert_eq!(parsed, url::Url::parse(as_str).unwrap());
+            assert_eq!(parsed, earl::Url::parse(as_str).unwrap());
         }
     }
 });
